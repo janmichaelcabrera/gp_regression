425454df574ca9bd77c5dc59ea62552330d9ec6a
janmichaelcabrera <janmichael.cabrera@gmail.com>
2020-03-03 12:27:43 -0600

    gp regression framework implemented

--- covariance_functions.py
+++ covariance_functions.py
@@ -7,31 +7,14 @@ from scipy.spatial import distance_matrix
 
 class covariance_functions:
     """
-    This class contains covariance function for Gaussian process smoothing
+    This class contains covariance function for Gaussian process regression
     """
-    def __init__(self):
-        pass
+    def __init__(self, b, tau_1, tau_2=10**-6):
+        self.b = b
+        self.tau_1 = tau_1
+        self.tau_2 = tau_2
 
-    def squared_exponential(x_1, x_2, hyperparams):
-        """
-        Parameters
-        ----------
-            x: float (vector)
-                Vector of points
-
-        Returns
-        ----------
-            C: float (matrix)
-                Returns a Matern (5,2) square covariance matrix of size(x)
-                .. math:: C_{SE}(x_1, x_2) = \\tau_1^2 e^{-\\frac{1}{2} (d/b)^2} + \\tau_2^2 \\delta(x_1, x_2)
-        """
-
-        # Unpack hypereparameters
-        b, tau_1_squared, tau_2_squared = hyperparams
-
-        # Initialize covariance matrix
-        C = np.zeros((x_1.shape[0], x_2.shape[0]))
-
+    def squared_exponential(self, x_1, x_2):
         if len(x_1.shape) == 1:
             X_1 = [[i] for i in x_1]
             X_2 = [[i] for i in x_2]
@@ -39,84 +22,123 @@ class covariance_functions:
             X_1 = x_1
             X_2 = x_2
 
-        C = distance_matrix(X_1, X_2)
+        D = distance_matrix(X_1, X_2)
 
-        C = tau_1_squared*np.exp(-(1/2)*(C/b)**2)
+        C = self.tau_1*np.exp(-(1/2)*(D/self.b)**2)
 
         if np.array_equal(x_1, x_2) == True:
-            C = C + tau_2_squared*np.eye(x_1.shape[0])
+            C = C + self.tau_2*np.eye(x_1.shape[0])
 
         return C
 
-    def matern_32(x_1, x_2, hyperparams):
-        """
-        Parameters
-        ----------
-            x: float (vector)
-                Vector of points
+    # def __init__(self):
+    #     pass
 
-        Returns
-        ----------
-            C: float (matrix)
-                Returns a Matern (5,2) square covariance matrix of size(x)
-                .. math:: C_{5,2}(x_1, x_2) = \\tau_1^2 [1 + \\sqrt{5} d / b + (5/3) (d/b)^2 ] e^{-\\sqrt{5} (d/b)} + \\tau_2^2 \\delta(x_1, x_2)
-        """
+    # def squared_exponential(x_1, x_2, hyperparams):
+    #     """
+    #     Parameters
+    #     ----------
+    #         x: float (vector)
+    #             Vector of points
 
-        # Unpack hypereparameters
-        b, tau_1_squared, tau_2_squared = hyperparams
+    #     Returns
+    #     ----------
+    #         C: float (matrix)
+    #             Returns a Matern (5,2) square covariance matrix of size(x)
+    #             .. math:: C_{SE}(x_1, x_2) = \\tau_1^2 e^{-\\frac{1}{2} (d/b)^2} + \\tau_2^2 \\delta(x_1, x_2)
+    #     """
 
-        # Initialize covariance matrix
-        C = np.zeros((x_1.shape[0], x_2.shape[0]))
+    #     # Unpack hypereparameters
+    #     b, tau_1_squared, tau_2_squared = hyperparams
 
-        if len(x_1.shape) == 1:
-            X_1 = [[i] for i in x_1]
-            X_2 = [[i] for i in x_2]
-        else:
-            X_1 = x_1
-            X_2 = x_2
+    #     # Initialize covariance matrix
+    #     C = np.zeros((x_1.shape[0], x_2.shape[0]))
 
-        C = distance_matrix(X_1, X_2)
+    #     if len(x_1.shape) == 1:
+    #         X_1 = [[i] for i in x_1]
+    #         X_2 = [[i] for i in x_2]
+    #     else:
+    #         X_1 = x_1
+    #         X_2 = x_2
 
-        C = tau_1_squared*(1 + np.sqrt(3)*(C/b))*np.exp(-np.sqrt(3)*(C/b))
+    #     C = distance_matrix(X_1, X_2)
 
-        if np.array_equal(x_1, x_2) == True:
-            C = C + tau_2_squared*np.eye(x_1.shape[0])
+    #     C = tau_1_squared*np.exp(-(1/2)*(C/b)**2)
 
-        return C
+    #     if np.array_equal(x_1, x_2) == True:
+    #         C = C + tau_2_squared*np.eye(x_1.shape[0])
 
-    def matern_52(x_1, x_2, hyperparams):
-        """
-        Parameters
-        ----------
-            x: float (vector)
-                Vector of points
+    #     return C
 
-        Returns
-        ----------
-            C: float (matrix)
-                Returns a Matern (5,2) square covariance matrix of size(x)
-                .. math:: C_{5,2}(x_1, x_2) = \\tau_1^2 [1 + \\sqrt{5} d / b + (5/3) (d/b)^2 ] e^{-\\sqrt{5} (d/b)} + \\tau_2^2 \\delta(x_1, x_2)
-        """
+    # def matern_32(x_1, x_2, hyperparams):
+    #     """
+    #     Parameters
+    #     ----------
+    #         x: float (vector)
+    #             Vector of points
 
-        # Unpack hypereparameters
-        b, tau_1_squared, tau_2_squared = hyperparams
+    #     Returns
+    #     ----------
+    #         C: float (matrix)
+    #             Returns a Matern (5,2) square covariance matrix of size(x)
+    #             .. math:: C_{5,2}(x_1, x_2) = \\tau_1^2 [1 + \\sqrt{5} d / b + (5/3) (d/b)^2 ] e^{-\\sqrt{5} (d/b)} + \\tau_2^2 \\delta(x_1, x_2)
+    #     """
+
+    #     # Unpack hypereparameters
+    #     b, tau_1_squared, tau_2_squared = hyperparams
+
+    #     # Initialize covariance matrix
+    #     C = np.zeros((x_1.shape[0], x_2.shape[0]))
+
+    #     if len(x_1.shape) == 1:
+    #         X_1 = [[i] for i in x_1]
+    #         X_2 = [[i] for i in x_2]
+    #     else:
+    #         X_1 = x_1
+    #         X_2 = x_2
+
+    #     C = distance_matrix(X_1, X_2)
+
+    #     C = tau_1_squared*(1 + np.sqrt(3)*(C/b))*np.exp(-np.sqrt(3)*(C/b))
+
+    #     if np.array_equal(x_1, x_2) == True:
+    #         C = C + tau_2_squared*np.eye(x_1.shape[0])
+
+    #     return C
+
+    # def matern_52(x_1, x_2, hyperparams):
+    #     """
+    #     Parameters
+    #     ----------
+    #         x: float (vector)
+    #             Vector of points
+
+    #     Returns
+    #     ----------
+    #         C: float (matrix)
+    #             Returns a Matern (5,2) square covariance matrix of size(x)
+    #             .. math:: C_{5,2}(x_1, x_2) = \\tau_1^2 [1 + \\sqrt{5} d / b + (5/3) (d/b)^2 ] e^{-\\sqrt{5} (d/b)} + \\tau_2^2 \\delta(x_1, x_2)
+    #     """
+
+    #     # Unpack hypereparameters
+    #     b, tau_1_squared, tau_2_squared = hyperparams
 
 
-        # Initialize covariance matrix
-        C = np.zeros((x_1.shape[0], x_2.shape[0]))
+    #     # Initialize covariance matrix
+    #     C = np.zeros((x_1.shape[0], x_2.shape[0]))
 
-        if len(x_1.shape) == 1:
-            X_1 = [[i] for i in x_1]
-            X_2 = [[i] for i in x_2]
-        else:
-            X_1 = x_1
-            X_2 = x_2
+    #     if len(x_1.shape) == 1:
+    #         X_1 = [[i] for i in x_1]
+    #         X_2 = [[i] for i in x_2]
+    #     else:
+    #         X_1 = x_1
+    #         X_2 = x_2
 
-        C = distance_matrix(X_1, X_2)
+    #     C = distance_matrix(X_1, X_2)
 
-        C = tau_1_squared*(1 + np.sqrt(5)*(C/b) + (5/3)*(C/b)**2)*np.exp(-np.sqrt(5)*(C/b))
+    #     C = tau_1_squared*(1 + np.sqrt(5)*(C/b) + (5/3)*(C/b)**2)*np.exp(-np.sqrt(5)*(C/b))
 
-        if np.array_equal(x_1, x_2) == True:
-            C = C + tau_2_squared*np.eye(x_1.shape[0])
+    #     if np.array_equal(x_1, x_2) == True:
+    #         C = C + tau_2_squared*np.eye(x_1.shape[0])
 
-        return C
+    #     return C
--- gaussian_process.py
+++ gaussian_process.py
@@ -3,6 +3,7 @@
 from __future__ import division
 import numpy as np
 import warnings
+from .covariance_functions import *
 from scipy.stats import multivariate_normal
 from scipy.spatial.distance import euclidean
 from scipy.spatial import distance_matrix
@@ -14,7 +15,7 @@ class gaussian_process:
     """
     This class returns a vector of smoothed values given feature and response vectors
     """
-    def __init__(self, x, hyperparams=[10, 10, 10**-6], y = [], x_star = [], cov='matern_52'):
+    def __init__(self, x_star, x, y, sigma_squared, cov_obj, cov_func='squared_exponential'):
         """
         Parameters
         ----------
@@ -35,185 +36,86 @@ class gaussian_process:
                     Squared exponential = 'squared_exponential'
                     Matern 5/2 = 'matern_52'
                     Matern 3/2 = 'matern_32'
-
         """
-        self.x = x
-        self.hyperparams = hyperparams
-        self.cov = cov
-        self.y = y
         self.x_star = x_star
+        self.x = x
+        self.y = y
+        self.sigma_squared = sigma_squared
+        self.model_error = self.sigma_squared*np.eye(self.x.shape[0])
+        # if np.isscalar(self.sigma_squared) :
+        #     self.model_error = self.sigma_squared*np.eye(self.x.shape[0])
+        # elif len(self.sigma_squared) == self.x.shape[0]:
+        #     self.model_error = np.diag(self.sigma_squared)
+        # else:
+        #     raise ValueError('Feature shape, %i and error shape, %i mismatch: '%self.x.shape[0], len(self.sigma_squared))
+        self.cov_obj = cov_obj
+        self.cov_func = cov_func
+
+        self.calc_cov_components()
 
 
+    def calc_cov_components(self):
+        self.C_x_star_x = getattr(self.cov_obj, self.cov_func)(self.x_star, self.x)
+        self.C_xx = getattr(self.cov_obj, self.cov_func)(self.x, self.x)
+        self.C_star_star = getattr(self.cov_obj, self.cov_func)(self.x_star, self.x_star)
 
-    def approx_var(self):
-        """
-        Returns
-        ----------
-            variance: float(scalar)
-                Approximates the variance for a given problem using residual sum of squared errors. The function runs a Gaussian Process once for the approximation
-
-        Attributes
-        ----------
-            residuals: float, len(y)
-                residuals of prediction from data
-                .. math:: r_i = y_i - \\hat{y}_i
-        """
-
-        # Evaluate covariance
-        C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-
-        # Assume initial estimate for variance
-        variance = 1
-
-        # Calculate weights
-        weights = C_xx @ inv(C_xx + variance*np.eye(self.x.shape[0]))
-
-        # Calculate predictions from GP
-        y_star = np.transpose(weights) @ self.y
-
-        # Calculate residuals from the above prediction
-        self.residuals = self.y - y_star
-
-        # Residual sum of squared errors
-        rss = (self.residuals**2).sum()
-
-        # Approximate standard error of fit
-        return rss/(len(y_star)-1)
-
-    def smoother(self, variance=[]):
-        """
-        Parameters
-        ----------
-            variance: float (scalar; optional)
-                If not specified, the sample variance is assumed to be one
-
-        Returns
-        ----------
-            y_star: float, len(x_star)
-                Predictor for x_star, 
-                .. math:: y^* = W^T y
-                .. math:: W = C(x^*, x) (C(x, x) + \\sigma^2 I)^{-1}
-
-            post_var: float, len(x_star)
-                Posterior variance given the observed data
-                .. math:: var[f(x^*)| y] = C(x^*, x^*) - C(x^*, x) ( C(x, x) + \\sigma^2 I)^{-1} C(x^*, x)^T
-        """
-        # If a sample variance is not spcified, assume it is 1
-        if not variance:
-            variance = 1
-
-        # Evaluate C(x^*, x)
-        C_x_star_x = getattr(covariance_functions, self.cov)(self.x_star, self.x, self.hyperparams)
-        # Evaluate C(x, x)
-        C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-        # Evaluate C(x^*, x^*)
-        C_star_star = getattr(covariance_functions, self.cov)(self.x_star, self.x_star, self.hyperparams)
-
+    def calc_mean_cov(self):
         # Calculate weights matrix,  W = C(x^*, x) (C(x, x) + \\sigma^2 I)^{-1}
-        weights = C_x_star_x @ inv(C_xx + variance*np.eye(self.x.shape[0]))
-        
-        # Calculate y_star, y^* = W^T y
-        y_star = np.transpose(weights) @ self.y
+        self.M = inv(self.C_xx + self.model_error)
 
         # Calculates posterior variance, var[f(x^*)| y] = C(x^*, x^*) - C(x^*, x) ( C(x, x) + \\sigma^2 I)^{-1} C(x^*, x)^T
-        self.post_cov = C_star_star - C_x_star_x @ inv(C_xx + variance*np.eye(self.x.shape[0])) @ np.transpose(C_x_star_x)
+        self.post_cov = self.C_star_star - self.C_x_star_x @ self.M @ np.transpose(self.C_x_star_x)
 
-        self.post_mean = inv(C_xx + variance*np.eye(self.x.shape[0])) @ C_x_star_x.T
+        weights = self.C_x_star_x @ self.M
+        # Calculate y_star, y^* = W^T y
+        self.post_mean = np.transpose(weights) @ self.y
 
-        post_var = np.diag(self.post_cov)
-        return y_star, post_var
+    def regression(self):
+        self.calc_mean_cov()
+        self.post_var = np.diag(self.post_cov)
+        return self.post_mean, self.post_var
 
-    def log_marginal_likelihood(self, hyperparams, variance=[]):
-        """
-        Parameters
-        ----------
-            hyperparams: float (tuple)
-                Hyperparameters for a Guassian process
+    def log_marginal_likelihood(self):
+        return multivariate_normal.logpdf(self.y, cov=(self.model_error + self.C_xx))
 
-            variance: float (scalar; optional)
-                If not specified, the sample variance is assumed to be one
 
-        Returns
-        ----------
-            p_y: float
-                Returns the log marginal-likelihood of a multivariate gaussian evaluated at the hyperparams
-                .. math: y \\sim N(0, \\sigma^2 I + C)
-        """
-        if not variance:
-            variance = 1
-
-        # Unpack hypereparameters
-        b, tau_1_squared, tau_2_squared = hyperparams
-
-        # Evaluate C(x, x)
-        C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, hyperparams)
-
-        # Evaluates covariance of marginal-likelihood: \sigma^2 I + C
-        covariance = variance*np.eye(self.x.shape[0]) + C_xx
-        return multivariate_normal.logpdf(self.y, cov=covariance)
-
-    def optimize_lml(self):
+    def optimize_lml(self, bounds=[(10**-3, 10**3),(10**-3, 10**5)]):
         """
         Returns
         ----------
             hyperparams: float (tuple)
                 Returns optimized b and tau_1_squared of hyperparameters. Optimized using BFGS from scipy.optimize.minimize method.
         """
-        # Unpacks hyperparameters
-        b, tau_1_squared, tau_2_squared = self.hyperparams
+        # Initial params
+        b, tau_1 = self.cov_obj.b, self.cov_obj.tau_1
+        params = [b, tau_1]
 
         # Wrapper function used to optimize the log marginal-likelihood
         def func(params):
             # Unpacks parameters to be optimized
-            b, tau_1_squared = params
-            variance = 1
+            b, tau_1 = params
 
-            # Repacks paramers
-            hyperparams = b, tau_1_squared, 10**-6
+            temp_cov = covariance_functions(b, tau_1)
 
             # Evaluates covariance function
-            C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, hyperparams)
+            temp_C_xx = getattr(temp_cov, self.cov_func)(self.x, self.x)
 
             # Evaluates covariance of marginal-likelihood: \sigma^2 I + C
-            covariance = variance*np.eye(self.x.shape[0]) + C_xx
+            covariance = self.model_error + temp_C_xx
 
             # Returns the negative of the marginal log-likelihood multivariate normal (in order to maximize the evaluation)
             return -multivariate_normal.logpdf(self.y, cov=covariance)
 
         # Uses BFGS optimization function to find optimal b and tau_1_squared.     
-        res = minimize(func, [b, tau_1_squared], bounds=[(0.1, 10**3),(0.1, 10**5)])
+        res = minimize(func, [b, tau_1], bounds=bounds)
 
         # Unpacks results of the optimization step
-        b, tau_1_squared = res.x
+        self.cov_obj.b, self.cov_obj.tau_1 = res.x
 
-        # Updates the model hyperparameters
-        self.hyperparams = b, tau_1_squared, tau_2_squared
-        return self.hyperparams
+        self.calc_cov_components()
 
-    def generate_random_samples(self, mean=[]):
-        """
-        Parameters
-        ----------
-            mean: float, vector
-                Mean vector for multivariate normal, should have same shape as self.x. If not specified, assumed to be mean zero multivariate normal.
-
-        Returns
-        ----------
-            fx: float, len(x)
-                Returns random samples from a multivariate with specified mean
-
-        Raises
-        ----------
-            ValueError
-                If mean vector shape and x shape do not match
-        """
-
-        if not mean:
-            mean = np.zeros(self.x.shape[0])
-
-        if mean.shape[0] != self.x.shape[0]:
-            raise ValueError('Mean shape, %i  and x shape, %i do not match'%(mean.shape[0], self.x.shape[0]))
-
-        covariance = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-        fx = multivariate_normal.rvs(mean=mean, cov=covariance)
-        return fx
+    def rvs(self, size=None):
+        if not hasattr(self, 'M'):
+            self.calc_mean_cov()
+            print('test')
+        return stats.multivariate_normal.rvs(mean=self.post_mean, cov=self.post_cov, size=size)
